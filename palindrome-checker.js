18a8a29cd32b44c1d35876eb03c78cf50b8974a7
Stephanie Barnett <stephaniebarnett@Stephanies-MacBook-Pro.local>
2022-05-18 18:09:37 -0400

    Updated spelling from palindrone to palindrome

---
+++ palindrome-checker.js
@@ -0,0 +1,7 @@
+console.log("Does this work?");
+function palidromeChecker(string){
+    let isPalindrome = false;
+    console.log("I'm inside your function and will print to the console!");
+    return isPalindrome;
+}
+palindromeChecker()
