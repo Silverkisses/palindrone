e3f26c71911ebec54d384422a89988e178ff1e8e
Stephanie Barnett <stephaniebarnett@Stephanies-MacBook-Pro.local>
2022-05-18 18:49:09 -0400

    Created basic palindrome checker

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -4,8 +4,13 @@ function palindromeChecker(palindromeString){
     for (i = palindromeString.length-1; i>=0; i--){
         reverseString = reverseString + palindromeString[i];
     }
+    if(palindromeString === reverseString){
+        isPalindrome = true;
+    } else {
+        isPalindrome = false;
+    }
     return isPalindrome;
 }
-palindromeChecker("test");
+palindromeChecker("mom");
 console.log(isPalindrome);
 console.log(reverseString);
